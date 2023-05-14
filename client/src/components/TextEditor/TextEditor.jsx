9e1df395cae129fcf233c9c15567af61fd1d3366
Rohit-Gupta11 <grohit7711@gmail.com>
2023-05-14 16:24:44 +0530

    Establish connection between client and server using io

--- client/src/components/TextEditor/TextEditor.jsx
+++ client/src/components/TextEditor/TextEditor.jsx
@@ -1,7 +1,8 @@
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './TextEditor.css';
+import io from 'socket.io-client';
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,13 +17,26 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const [socket, setSocket] = useState();
+    const [quill, setQuill] = useState();
+
+    useEffect(() => {
+        const s = io(process.env.SERVER_URL);
+        setSocket(s);
+
+        return () => {
+            s.disconnect();
+        }
+    }, []);
+
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return;
         wrapper.innerHTML = '';
 
         const editor = document.createElement('div');
         wrapper.append(editor);
-        new Quill(editor, { theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}});
+        const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+        setQuill(q);
     }, []);
 
     return (
--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: process.env.ORIGIN,
         methods: ["GET", "POST"]
     }
 });
+
+io.on('connection', socket => {
+    console.log('this is working well')
+})
