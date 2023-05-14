610be7abb3c05c2d2b0b5b5bdb4b405a60870ec1
Rohit-Gupta11 <grohit7711@gmail.com>
2023-05-14 19:07:51 +0530

    Implement socket io logics and mongoose to save data

--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require("mongoose")
+const Document = require("./models/Document")
 
-const io = require('socket.io')(3001, {
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('db')
+})
+
+const io = require("socket.io")(3001, {
     cors: {
         origin: process.env.ORIGIN,
-        methods: ["GET", "POST"]
-    }
+        methods: ["GET", "POST"],
+    },
+})
+
+const defaultValue = "";
+
+io.on("connection", socket => {
+    console.log('this is working well')
+    socket.on("get-document", async documentId => {
+        const document = await findOrCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit("load-document", document.data);
+
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        });
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        });
+    });
 });
 
-io.on('connection', socket => {
-    console.log('this is working well')
-})
+async function findOrCreateDocument(id) {
+    if (id == null) return;
+    
+    const document = await Document.findById(id);
+    
+    if (document) return document;
+    
+    return await Document.create({ _id: id, data: defaultValue });
+}
